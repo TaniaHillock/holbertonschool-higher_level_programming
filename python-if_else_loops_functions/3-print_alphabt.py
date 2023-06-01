eb98a55fbefe4274345bfc6a2e046ded6d0cf65b
TaniaHillock <tania.hillock@gmail.com>
2023-05-31 21:29:51 -0600

    alphabeth

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for charac in range(97, 123):
-        if charac != 101 and ch != 113:
+        if charac != 101 and charac != 113:
                 print("{}".format(chr(charac)), end='')

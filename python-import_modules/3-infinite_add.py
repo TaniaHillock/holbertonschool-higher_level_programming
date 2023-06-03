2fe283c3792d286d2a291982c25e0151e589250b
TaniaHillock <tania.hillock@gmail.com>
2023-06-03 08:43:28 -0600

    fix

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
-    result = 0
+    answer = 0
     for arg in sys.argv:
         if arg != sys.argv[0]:
-            result += int(arg)
+            answer += int(arg)
     print(result)
-    

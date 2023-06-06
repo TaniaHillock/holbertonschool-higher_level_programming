14e0aa2b583adfebad3456018ca6c918cee13f3f
TaniaHillock <tania.hillock@gmail.com>
2023-06-06 13:50:38 -0600

    Remove Cc

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     length = len(my_string)
-    j=0
+    j = 0
     new_string = my_string[:]
     for i in range(length):
         if (my_string[i] == 'c' or my_string[i] == 'C'):

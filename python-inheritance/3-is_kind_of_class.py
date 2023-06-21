717b7953bc19b6a972d6cce555ee247935ccdefe
TaniaHillock <tania.hillock@gmail.com>
2023-06-20 20:28:45 -0600

    Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module of the Function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Function that returns True/False if obj is an instance of a_class """
+    return isinstance(obj, a_class)

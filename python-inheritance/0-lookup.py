c6f6bc8b12a71d6e97d7544578f577b82ce50e4d
TaniaHillock <tania.hillock@gmail.com>
2023-06-20 20:13:56 -0600

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+"""Module for a function"""
+
+
 def lookup(obj):
     """ Function that returns the list of available attributes
-        and methods of an object
-    Args:
-        obj: instance of the class
-    Returns:
-        List of attributes
-    """
-
+        and methods of an object"""
     return dir(obj)

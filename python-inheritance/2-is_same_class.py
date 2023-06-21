322956bb19618bca1eb363811ddeb005dc863fd5
TaniaHillock <tania.hillock@gmail.com>
2023-06-20 20:25:28 -0600

    Exact same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module of the Function"""
+
+
+def is_same_class(obj, a_class):
+    """ Function that returns True/False if obj is a type of a_class"""
+    return type(obj) is a_class

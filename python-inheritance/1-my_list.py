292c2f44af2522e647316a4f9894f3f2d18e9025
TaniaHillock <tania.hillock@gmail.com>
2023-06-20 20:19:23 -0600

    Print a sorted list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+"""Module for a func"""
+
+
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-
-    Args:
-        list: class list
-
-    """
+    """ Class that inherits the attributes references of class list """
 
     def print_sorted(self):
         """ Method that prints the sorted list """
         l_sorted = self.copy()
         l_sorted.sort()
         print(l_sorted)
+        return(l_sorted)
